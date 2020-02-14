deea05953faa424e8ef1efc9509b54b7909ea5b5
Anthony <ant1ng@berkeley.edu>
2020-02-14 14:28:11 -0800

    Added tests

--- puzzlesolver/solver/PickleSolverWrapper.py
+++ puzzlesolver/solver/PickleSolverWrapper.py
@@ -1,22 +1,24 @@
 import pickle
 from .GeneralSolver import GeneralSolver
+import os
 
 class PickleSolverWrapper(GeneralSolver):
 
-    def __init__(self, puzzle=None):
+    def __init__(self, puzzle=None, path="."):
         try:
             assert puzzle
-            f = open(puzzle.__class__.__name__ + '.pkl')
+            f = open(path + "/" + puzzle.__class__.__name__ + '.pkl', 'rb')
             pkl = pickle.load(f)
             self.values, self.remoteness = pkl[0], pkl[1]
             f.close()
-        except:
-            print("WARNING: Error opening file")
+        except FileNotFoundError:
+            print("WARNING: File not found, intializing new memory storage")
             GeneralSolver.__init__(self, puzzle=puzzle)
+        self.path = path
 
     def solve(self, puzzle):
         output = GeneralSolver.solve(self, puzzle)
-        f = open(puzzle.__class__.__name__ + ".pkl", 'wb')
+        f = open(self.path + "/" + puzzle.__class__.__name__ + ".pkl", 'wb')
         pickle.dump([self.values, self.remoteness], f)
         f.close()
         return output
--- tests/testHanoi.py
+++ tests/testHanoi.py
@@ -2,11 +2,29 @@ import pytest
 
 from puzzlesolver.puzzles.Hanoi import Hanoi
 from puzzlesolver.solver.PickleSolverWrapper import PickleSolverWrapper
+from puzzlesolver.solver.GeneralSolver import GeneralSolver
 from puzzlesolver.util import *
+
+import tempfile
+import os
 
 def testGeneral():
     for i in range(5):
         puzzle = Hanoi(size=i)
-        solver = PickleSolverWrapper()
+        solver = GeneralSolver(puzzle=puzzle)
         assert solver.solve(puzzle) == PuzzleValue.SOLVABLE
         assert solver.getRemoteness(puzzle) == 2**i - 1
+
+def testPickleWrapper():
+    i = 5
+    with tempfile.TemporaryDirectory() as directory:
+        puzzle = Hanoi(size=i)
+        solver = PickleSolverWrapper(puzzle=puzzle, path=directory)
+        assert not solver.values and not solver.remoteness
+        assert solver.solve(puzzle) == PuzzleValue.SOLVABLE
+        assert solver.values and solver.remoteness
+
+        puzzle = Hanoi(size=i)
+        solver = PickleSolverWrapper(puzzle=puzzle, path=directory)
+        assert solver.values and solver.remoteness
+        assert solver.solve(puzzle) == PuzzleValue.SOLVABLE
