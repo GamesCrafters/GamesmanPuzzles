58242ee75a42b20eeded0b8a8f728f2fa79d16b7
Anthony Ling <ant1ng@berkeley.edu>
2020-03-14 11:19:36 -0700

    Started server.py file

---
+++ server.py
@@ -0,0 +1,62 @@
+import flask
+from flask import request, jsonify
+from puzzlesolver.puzzles.hanoi import Hanoi
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+puzzleList = {
+    'hanoi': Hanoi
+}
+
+@app.route('/puzzles', methods=['GET'])
+def puzzles():
+    response = {
+        "available puzzles": list(puzzleList.keys())
+    }
+    return jsonify(response)
+
+@app.route('/puzzles/<puzzle_name>', methods=['GET'])
+def puzzle(puzzle_name):
+    if puzzle_name not in puzzleList: 
+        return "Error"
+    response = {
+        "puzzle name": puzzle_name,
+        "variants": list(puzzleList[puzzle_name].variants.keys())
+    }
+    return jsonify(response)
+
+@app.route('/puzzles/<puzzle_name>/<variant_id>', methods=['GET'])
+def puzzle_variant(puzzle_name, variant_id):
+    if puzzle_name not in puzzleList:
+        return "Error"
+    if int(variant_id) not in puzzleList[puzzle_name].variants.keys():
+        return "Error"
+    p = puzzleList[puzzle_name]()
+    response = {
+        "puzzle name": puzzle_name,
+        "variant_id": int(variant_id),
+        "starting_pos": p.serialize()
+    }
+    return jsonify(response)
+
+@app.route('/puzzles/<puzzle_name>/<variant_id>/<position>', methods=['GET'])
+def puzzle_position(puzzle_name, variant_id, position):
+    variant_id = int(variant_id)
+    if puzzle_name not in puzzleList:
+        return "Error"
+    if int(variant_id) not in puzzleList[puzzle_name].variants.keys():
+        return "Error"
+    p = puzzleList[puzzle_name].deserialize(position)
+    s = puzzleList[puzzle_name].variants[variant_id](p)
+    s.solve()
+    response = {
+        "puzzle name": puzzle_name,
+        "variant_id": int(variant_id),
+        "position": p.serialize(),
+        "remoteness": s.getRemoteness(p),
+        "value": s.getValue(p)
+    }
+    return jsonify(response)
+
+app.run()
