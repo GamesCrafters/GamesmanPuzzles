21c94dc685b1dbb07bd14e58e59f515457cea764
Arturo Olvera <olvera@berkeley.edu>
2020-04-13 21:10:35 -0500

    npuzzle server commit (pending more validation)

--- puzzlesolver/puzzles/__init__.py
+++ puzzlesolver/puzzles/__init__.py
@@ -2,9 +2,11 @@ from ._models import *
 
 from .hanoi import Hanoi
 from .graphpuzzle import GraphPuzzle
+from .npuzzle import Npuzzle
 
 puzzleList = {
-    Hanoi.puzzleid: Hanoi
+    Hanoi.puzzleid: Hanoi,
+    Npuzzle.puzzleid: Npuzzle
 }
 
 for puzzle in puzzleList.values():
---
+++ tests/puzzles/test_Npuzzle.py
@@ -0,0 +1,33 @@
+import pytest
+
+from puzzlesolver.puzzles import Npuzzle
+from puzzlesolver.solvers import GeneralSolver, sqlitesolver
+
+def testRemotenessSanity():
+    for i in range(2,4):
+        puzzle = Npuzzle(size=i)
+        solver = GeneralSolver(puzzle=puzzle)
+        solver.solve()
+        assert solver.getValue(puzzle) == PuzzleValue.SOLVABLE
+        assert solver.getRemoteness(puzzle) == 1
+
+def testSerialization():
+    for i in range(2,4):
+        puzzle = Npuzzle(size=i)
+        solver = GeneralSolver(puzzle=puzzle)
+        solver.solve()
+        new_puzzle = Npuzzle.deserialize(puzzle.serialize())
+        assert hash(puzzle) == hash(new_puzzle)
+
+def testValidation():
+    invalid_puzzle = "1-2-3-3"
+    valid_puzzle = "1-2-3-0"
+    blank_puzzle = ""
+    weird_input = "123---"
+    pytest.raises(PuzzleException, Npuzzle.validate, blank_puzzle, "2")
+    pytest.raises(PuzzleException, Npuzzle.validate, weird_input, "2")
+    pytest.raises(PuzzleException, Npuzzle.validate, invalid_puzzle, "2")
+    pytest.raises(PuzzleException, Npuzzle.validate, valid_puzzle, "3")
+    Npuzzle.validate(valid_puzzle, "2")
+
+    
