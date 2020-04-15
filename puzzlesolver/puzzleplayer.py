a3d8081a73974072329d757ee10460fe1d5906bb
Anthony Ling <ant1ng@berkeley.edu>
2020-04-15 14:35:08 -0700

    Added best move and solver info

--- puzzlesolver/puzzleplayer.py
+++ puzzlesolver/puzzleplayer.py
@@ -5,11 +5,13 @@ from .util import *
 
 class PuzzlePlayer:
 
-    def __init__(self, puzzle, solver=None, auto=False):
+    def __init__(self, puzzle, solver=None, solverinfo=True, auto=False, bestmove=False):
         self.base = puzzle
         self.puzzle = puzzle
         self.solver = solver
+        self.solverinfo = solverinfo
         self.auto = auto
+        self.bestmove = bestmove
         if solver:
             self.solver.solve()
 
@@ -28,18 +30,23 @@ class PuzzlePlayer:
     def printInfo(self):
         print("Turn:          ", self.turn), 
         print("Primitive:     ", self.puzzle.primitive())
-        if self.solver:
+        if self.solverinfo and self.solver:
             print("Solver:        ", self.solver.getValue(self.puzzle))
             print("Remoteness:    ", self.solver.getRemoteness(self.puzzle))
+        if self.bestmove: print("Best Move:     ", self.generateBestMove())
         self.turn += 1
 
     # Prompts for input and moves
     def printTurn(self):
-        if self.auto: 
-            move = self.generateBestMove()
-            self.puzzle = self.puzzle.doMove(move)
+        move = self.generateBestMove()
+        # Auto generate a possible solution
+        if self.auto: self.puzzle = self.puzzle.doMove(move)
         else:
             moves = list(self.puzzle.generateMoves(movetype="legal"))
+            # Have the best move be the first index
+            if self.bestmove: 
+                moves.remove(move)
+                moves.insert(0, move)
             print("Possible Moves:")
             for count, m in enumerate(moves):
                 print(str(count) + " -> " + str(m))
@@ -53,6 +60,8 @@ class PuzzlePlayer:
 
     # Generates best move from the solver
     def generateBestMove(self):
+        if self.solver.getValue(self.puzzle) == PuzzleValue.UNSOLVABLE: return None
+        if self.puzzle.primitive() == PuzzleValue.SOLVABLE: return None
         remotes = {
             self.solver.getRemoteness(self.puzzle.doMove(move)) : move 
             for move in self.puzzle.generateMoves(movetype="legal")
@@ -60,4 +69,3 @@ class PuzzlePlayer:
         if PuzzleValue.UNSOLVABLE in remotes:
             del remotes[PuzzleValue.UNSOLVABLE]
         return remotes[min(remotes.keys())]
-
