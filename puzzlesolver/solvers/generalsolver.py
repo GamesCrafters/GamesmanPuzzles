e7b111f7e84f97770015a7aa933574fae650ee8c
Anthony Ling <ant1ng@berkeley.edu>
2020-04-28 10:06:05 -0700

    Progressbar

--- puzzlesolver/solvers/generalsolver.py
+++ puzzlesolver/solvers/generalsolver.py
@@ -1,6 +1,7 @@
 from .solver import Solver
 from ..util import *
 import queue as q
+import progressbar
 
 class GeneralSolver(Solver):
 
@@ -14,11 +15,14 @@ class GeneralSolver(Solver):
         if hash(puzzle) in self.remoteness: return self.remoteness[hash(puzzle)]
         return PuzzleValue.UNSOLVABLE
 
-    def solve(self, *args, **kwargs):
+    def solve(self, *args, verbose=False, **kwargs):
         """Traverse the entire puzzle tree and classifiers all the 
         positions with values and remoteness
         - If position already exists in memory, returns its value
-        """        
+        """
+        if verbose: 
+            bar = progressbar.ProgressBar()
+            if hasattr(self.puzzle, 'numPositions'): bar.max_value = self.puzzle.numPositions
         solutions, queue = self.puzzle.generateSolutions(), q.Queue()
         for solution in solutions: 
             self.remoteness[hash(solution)] = 0
@@ -26,9 +30,11 @@ class GeneralSolver(Solver):
 
         # BFS for remoteness classification                        
         while not queue.empty():
+            if verbose: bar.update(len(self.remoteness) + 1)
             puzzle = queue.get()
             for move in puzzle.generateMoves('undo'):
                 nextPuzzle = puzzle.doMove(move)
                 if hash(nextPuzzle) not in self.remoteness:
                     self.remoteness[hash(nextPuzzle)] = self.remoteness[hash(puzzle)] + 1
                     queue.put(nextPuzzle)
+        if verbose: bar.finish()
