06c09b4fc6baa4182c7b8b5fa868eb2660ef87ec
Brian <briancdelaney2018@gmail.com>
2020-11-19 21:58:35 -0800

    First draft of CSP solver - naive approach

--- puzzlesolver/puzzles/_models/puzzle.py
+++ puzzlesolver/puzzles/_models/puzzle.py
@@ -7,7 +7,8 @@ class Puzzle(ABC):
     
     # Intializer
     def __init__(self, **kwargs):
-        pass
+		# Change this to True if you cannot generate solutions
+        self.noGenerateSolutions = False
 
     # Gameplay methods
     @abstractmethod
@@ -83,15 +84,6 @@ class Puzzle(ABC):
         In that case, the hash of all those specific permutations are the same.
         """
         raise NotImplementedError
-    
-    @abstractmethod
-    def generateSolutions(self, **kwargs):
-        """Returns a Iterable of Puzzle objects that are solved states
-
-        Outputs:
-            Iterable of Puzzles
-        """
-        raise NotImplementedError
 
     @property
     def numPositions(self):
@@ -115,6 +107,15 @@ class Puzzle(ABC):
         Can be custom defined"""
 
         print(str(self))
+        
+    def generateSolutions(self, **kwargs):
+        """Returns a Iterable of Puzzle objects that are solved states.
+        Not required if noGenerateSolutions is true, and using a CSP-implemented solver.
+
+        Outputs:
+            Iterable of Puzzles
+        """
+        raise NotImplementedError
 
     def generateMovePositions(self, movetype="legal", **kwargs):
         """Generate an iterable of puzzles with all moves fitting movetype
