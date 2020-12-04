d30015f30ce8972dbd03c043c824ccc22e2a5201
Brian <briancdelaney2018@gmail.com>
2020-12-03 21:29:28 -0800

    Test for CSP solver implemented

--- tests/solvers/test_GeneralSolver.py
+++ tests/solvers/test_GeneralSolver.py
@@ -6,3 +6,22 @@ from puzzlesolver.util import *
 
 def testSimple(simple):
     simple(GeneralSolver)
+    
+def testCSP():
+    forward = GraphPuzzle(0, csp=True)
+    bidirectional = GraphPuzzle(1, csp=True)
+    backward = GraphPuzzle(2, csp=True)
+    sol = GraphPuzzle(3, value=PuzzleValue.SOLVABLE, csp=True)
+
+    sol.setMove(forward, movetype="for")
+    sol.setMove(bidirectional, movetype="bi")
+    sol.setMove(backward, movetype="back")
+
+    solver = GeneralSolver(sol)
+    solver.solve()
+
+    assert solver.getRemoteness(backward) == 1
+    assert solver.getRemoteness(sol) == 0
+    assert solver.getRemoteness(bidirectional) == 1
+    assert solver.getRemoteness(forward) == PuzzleValue.UNSOLVABLE
+    print("CSP test: Successful")
