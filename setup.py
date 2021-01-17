753d73f46c80f9f1d53f9f22b802dfa1fc686914
Anthony Ling <ant1ng@berkeley.edu>
2021-01-16 21:24:27 -0800

    find packages instead of puzzlesolver

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup, Extension
+from setuptools import setup, Extension, find_packages
 
 setup(
     name="example-pkg-ant1ng-puzzlesolver",
     version="1.0.0",
-    packages=['puzzlesolver'],
+    packages=find_packages(),
     ext_modules=[
         Extension(
             "puzzlesolver.python._puzzlesolverc", 
@@ -17,5 +17,13 @@ setup(
                 "puzzlesolver/c/src/Hanoi.c"
             ]
         )
+    ],
+    install_requires=[
+        'Flask>=1.1.1',
+        'pytest>=5.3.5'
+        'sqlitedict>=1.6.0',
+        'progressbar2>=3.51.0',
+        'networkx>=2.4',
     ]
+
 )
