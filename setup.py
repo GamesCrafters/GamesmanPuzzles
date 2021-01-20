74320c662d99cea2ef138f7145fdf395c13e1046
Anthony Ling <ant1ng@berkeley.edu>
2021-01-19 18:17:09 -0800

    Updated package to have an accelerator program for only linux

--- setup.py
+++ setup.py
@@ -1,18 +1,20 @@
-from setuptools import setup, Extension, find_packages
+import setuptools
+import sys
 
-setup(
-    name="GamesmanPuzzles",
-    version="0.0.3",
-    packages=find_packages(),
-    install_requires=[
+install_requires = None
+ext_modules = None
+
+# CPython extension, (only tested on Linux however)
+if sys.platform.startswith('linux'):
+    install_requires = [
         'Flask>=1.1.1',
         'pytest>=5.3.5',
         'sqlitedict>=1.6.0',
         'progressbar2>=3.51.0',
         'networkx>=2.4',
-    ],
-    ext_modules=[
-        Extension(
+    ]
+    ext_modules = [
+        setuptools.Extension(
             "puzzlesolver._puzzlesolverc", 
             include_dirs=[
                 "puzzlesolver/puzzles/hanoi/include",
@@ -28,6 +30,14 @@ setup(
                 "puzzlesolver/puzzles/hanoi/src/Hanoi.c"
             ]
         )
-    ],
+    ]
+
+setuptools.setup(
+    name="GamesmanPuzzles",
+    version="0.0.4",
+    packages=setuptools.find_packages(),
+    description="A collection of puzzles and solvers under a simple but powerful interface",
+    install_requires=install_requires,
+    ext_modules=ext_modules,
     python_requires='>=3.6'
 )
