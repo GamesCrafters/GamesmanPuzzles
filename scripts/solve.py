6d62cf622bde93afc9dd738272affe6d3046cb12
Anthony Ling <40441724+Ant1ng2@users.noreply.github.com>
2021-12-25 12:27:21 -0800

    Added new scripts directory to store solver and server scripts (as well as a config file) (#116)

---
+++ scripts/solve.py
@@ -0,0 +1,27 @@
+from puzzlesolver.puzzles import PuzzleManager
+
+import os
+
+from threading import Thread
+
+# Initalizes the data
+def init_data():
+    for p_cls in PuzzleManager.getPuzzleClasses():        
+        if data["TESTING"]:
+            variants = p_cls.test_variants
+        else:
+            variants = p_cls.variants
+        for variant in variants:
+            s_cls = PuzzleManager.getSolverClass(p_cls.id, variant)
+            puzzle = p_cls.generateStartPosition(variant)
+            solver = s_cls(puzzle, dir_path=data['DATABASE_DIR'])
+            solver.solve(verbose=True)
+
+
+if __name__ == "__main__":
+    import json
+
+    with open("config.json") as json_data_file:
+        data = json.load(json_data_file)
+
+    init_data()
