5c4ad8f81de83ba8f25db6f21c60ad86cc354c7d
Cameron Cheung <cameronlcheung@yahoo.com>
2022-10-21 02:40:57 -0700

    CORS

--- scripts/server/src/routes.py
+++ scripts/server/src/routes.py
@@ -1,5 +1,6 @@
 import flask
 from flask import request, jsonify, abort
+from flask_cors import CORS
 from puzzlesolver.puzzles import PuzzleManager
 from puzzlesolver.util import PuzzleException, PuzzleValue
 
@@ -15,6 +16,8 @@ app.config["DEBUG"] = False
 app.config["TESTING"] = False
 app.config['DATABASE_DIR'] = 'databases'
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+
+CORS(app)
 
 def check_available(puzzle_id, variant=None):
 
