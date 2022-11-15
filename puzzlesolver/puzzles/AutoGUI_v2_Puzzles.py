78cd0234e836922cedc406760672bad236a12fc6
Mia Pulido Campdera <miapulidocampdera@Mias-MacBook-Pro.local>
2022-11-15 13:12:20 -0800

    hanoi v2 3_3

--- puzzlesolver/puzzles/AutoGUI_v2_Puzzles.py
+++ puzzlesolver/puzzles/AutoGUI_v2_Puzzles.py
@@ -1,50 +1,49 @@
 def getNQueens(variant_id):
-    #if variant_id not in (4,):
-    #    return None
-    return {
-            "defaultTheme": "regular",
-            "themes": {
-                "regular": {
-                    "backgroundGeometry": [4, 4],
-                    "backgroundImage": "nqueens/grid.svg",
-                    "centers": [[0.5 + (i % 4), 0.5 + (i // 4)] for i in range(16)],
-                    "pieces": {
-                        "q": {
-                            "image": "chess/Q.svg",
-                            "scale": 1
+    if variant_id == '4':
+        return {
+                "defaultTheme": "regular",
+                "themes": {
+                    "regular": {
+                        "backgroundGeometry": [4, 4],
+                        "backgroundImage": "nqueens/grid.svg",
+                        "centers": [[0.5 + (i % 4), 0.5 + (i // 4)] for i in range(16)],
+                        "pieces": {
+                            "q": {
+                                "image": "chess/Q.svg",
+                                "scale": 1
+                            }
                         }
                     }
                 }
             }
-        }
 
 def getHanoi(variant_id):
-    #if variant_id not in (4,):
-    #    return None
-    return {
-            "defaultTheme": "regular",
-            "themes": {
-                "regular": {
-                    "backgroundGeometry": [3, 3],
-                    "backgroundImage": "hanoi/3x3grid.svg",
-                    "centers": [[0.5 + (i % 3), 0.5 + (i // 3)] for i in range(9)],
-                    "pieces": {
-                        "A": {
-                            "image": "hanoi/A.svg",
-                            "scale": 1
-                        },
-                        "B": {
-                            "image": "hanoi/B.svg",
-                            "scale": 1
-                        },
-                        "C": {
-                            "image": "hanoi/C.svg",
-                            "scale": 1
+
+    if variant_id == '3_3':
+        return {
+                "defaultTheme": "regular",
+                "themes": {
+                    "regular": {
+                        "backgroundGeometry": [3, 3],
+                        "backgroundImage": "hanoi/3x3grid.svg",
+                        "centers": [[0.5 + (i % 3), 0.5 + (i // 3)] for i in range(9)],
+                        "pieces": {
+                            "A": {
+                                "image": "hanoi/A.svg",
+                                "scale": 1
+                            },
+                            "B": {
+                                "image": "hanoi/B.svg",
+                                "scale": 1
+                            },
+                            "C": {
+                                "image": "hanoi/C.svg",
+                                "scale": 1
+                            }
                         }
                     }
                 }
             }
-        }
 
 autoGUIv2DataFuncs = {
     "nqueens": getNQueens,
