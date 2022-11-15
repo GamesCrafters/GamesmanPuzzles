415ce473a7a55b7e93d489a8552b511ceecc5020
Mia Pulido Campdera <miapulidocampdera@Mias-MacBook-Pro.local>
2022-11-15 13:03:53 -0800

    hanoi v2 disks

--- puzzlesolver/puzzles/AutoGUI_v2_Puzzles.py
+++ puzzlesolver/puzzles/AutoGUI_v2_Puzzles.py
@@ -18,8 +18,37 @@ def getNQueens(variant_id):
             }
         }
 
+def getHanoi(variant_id):
+    #if variant_id not in (4,):
+    #    return None
+    return {
+            "defaultTheme": "regular",
+            "themes": {
+                "regular": {
+                    "backgroundGeometry": [3, 3],
+                    "backgroundImage": "hanoi/3x3grid.svg",
+                    "centers": [[0.5 + (i % 3), 0.5 + (i // 3)] for i in range(9)],
+                    "pieces": {
+                        "A": {
+                            "image": "hanoi/A.svg",
+                            "scale": 1
+                        },
+                        "B": {
+                            "image": "hanoi/B.svg",
+                            "scale": 1
+                        },
+                        "C": {
+                            "image": "hanoi/C.svg",
+                            "scale": 1
+                        }
+                    }
+                }
+            }
+        }
+
 autoGUIv2DataFuncs = {
-    "nqueens": getNQueens
+    "nqueens": getNQueens,
+    "hanoi": getHanoi
 }
 
 def get_autoguiV2Data(puzzle_id, variant_id):
