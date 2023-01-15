df32cbf3c2a0d9577877f1fbf6e3e1dca5a0c7a5
Robert <robertyishi@gmail.com>
2023-01-14 21:04:21 -0800

    revert

--- puzzlesolver/puzzles/__init__.py
+++ puzzlesolver/puzzles/__init__.py
@@ -3,6 +3,7 @@ from ..solvers import IndexSolver, PickleSolver, LightsOutClosedFormSolver
 from ..util import PuzzleException
 
 # Put your dependencies here
+from .graphpuzzle import GraphPuzzle
 from .hanoi import Hanoi
 from .lightsout import LightsOut
 from .pegSolitaire import Peg
